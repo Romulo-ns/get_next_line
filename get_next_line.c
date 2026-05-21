89e8da51cb7fa7c5f6282882ead546baaad6caeb
Romulo-ns <romulo.na.santos@gmail.com>
2026-05-21 19:23:51 +0100

    main functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: romdo-na <romdo-na@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 14:46:18 by romdo-na          #+#    #+#             */
-/*   Updated: 2026/05/21 15:58:31 by romdo-na         ###   ########.fr       */
+/*   Updated: 2026/05/21 19:23:12 by romdo-na         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,57 @@ char *get_next_line(int fd)
         /* get_next_line */
     }
     
+}
+char	*extract_line(char *stash)
+{
+	char	*line;
+	int		i;
+	int		j;
+
+	if (!stash || !stash[0])
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	line = malloc(i + 1);
+	if (!line)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		line[j] = stash[j];
+		j++;
+	}
+	line[j] = '\0';
+	return (line);
+}
+
+char	*read_and_save(int fd, char *stash)
+{
+	char	*buffer;
+	char	*tmp;
+	ssize_t	bytes_read;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(stash, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(buffer);
+			free(stash);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		tmp = ft_strjoin(stash, buffer);
+		free(stash);
+		stash = tmp;
+	}
+	free(buffer);
+	return (stash);
 }
